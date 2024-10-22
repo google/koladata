b227d812e6436a522c33c4c08751ff0291242478
Andrey Mikhaylov <amik@google.com>
2024-10-22 07:31:32 -0700

    Add kd_ext.py for convenience to import tools to work with external libraries.
    PiperOrigin-RevId: 688542400
    Change-Id: I9a594f35a17507fd1c3d5d1c390a3d79c3aad0c1

---
+++ py/koladata/ext/kd_ext_test.py
@@ -0,0 +1,28 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from koladata.ext import kd_ext
+
+
+class KdExtTest(absltest.TestCase):
+
+  def test_contains_modules(self):
+    modules = dir(kd_ext)
+    self.assertIn('npkd', modules)
+    self.assertIn('pdkd', modules)
+
+
+if __name__ == '__main__':
+  absltest.main()
