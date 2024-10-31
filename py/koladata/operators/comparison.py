e8d86a06224fd20860de8633526fda668fb3cb77
Di Lu <dilumich@google.com>
2024-10-31 09:07:43 -0700

    Add kde.full_equal
    PiperOrigin-RevId: 691824878
    Change-Id: I71a8f8884c068406dab44d502b85a7f401486753

--- py/koladata/operators/comparison.py
+++ py/koladata/operators/comparison.py
@@ -34,17 +34,43 @@ P = arolla.P
     qtype_inference_expr=qtypes.DATA_SLICE,
 )
 def equal(x, y):  # pylint: disable=unused-argument
-  """Returns true iff `x` and `y` are equal.
+  """Returns present iff `x` and `y` are equal.
 
   Pointwise operator which takes a DataSlice and returns a MASK indicating
   iff `x` and `y` are equal. Returns `kd.present` for equal items and
   `kd.missing` in other cases.
 
   Args:
-    x: DataSlice | DataItem.
-    y: DataSlice | DataItem.
+    x: DataSlice.
+    y: DataSlice.
   """
   raise NotImplementedError('implemented in the backend')
+
+
+@optools.add_to_registry(aliases=['kde.full_equal'])
+@optools.as_lambda_operator(
+    'kde.comparison.full_equal',
+    qtype_constraints=[
+        qtype_utils.expect_data_slice(P.x),
+        qtype_utils.expect_data_slice(P.y),
+    ],
+)
+def full_equal(x, y):  # pylint: disable=unused-argument
+  """Returns present iff all present items in `x` and `y` are equal.
+
+  The result is a zero-dimensional DataItem. Note that it is different from
+  `kd.all(x == y)`.
+
+  For example,
+    kd.full_equal(kd.slice([1, 2, 3]), kd.slice([1, 2, 3])) -> kd.present
+    kd.full_equal(kd.slice([1, 2, 3]), kd.slice([1, 2, None])) -> kd.missing
+    kd.full_equal(kd.slice([1, 2, None]), kd.slice([1, 2, None])) -> kd.present
+
+  Args:
+    x: DataSlice.
+    y: DataSlice.
+  """
+  return logical.all_((x == y) | (logical.has_not(x) & logical.has_not(y)))
 
 
 @optools.add_to_registry(
@@ -58,15 +84,15 @@ def equal(x, y):  # pylint: disable=unused-argument
     ],
 )
 def not_equal(x, y):
-  """Returns true iff `x` and `y` are not equal.
+  """Returns present iff `x` and `y` are not equal.
 
   Pointwise operator which takes a DataSlice and returns a MASK indicating
   iff `x` and `y` are not equal. Returns `kd.present` for not equal items and
   `kd.missing` in other cases.
 
   Args:
-    x: DataSlice | DataItem.
-    y: DataSlice | DataItem.
+    x: DataSlice.
+    y: DataSlice.
   """
   return ~(x == y) & (logical.has(x) & logical.has(y))
 
@@ -81,15 +107,15 @@ def not_equal(x, y):
     qtype_inference_expr=qtypes.DATA_SLICE,
 )
 def greater(x, y):  # pylint: disable=unused-argument
-  """Returns true iff `x` is greater than `y`.
+  """Returns present iff `x` is greater than `y`.
 
   Pointwise operator which takes a DataSlice and returns a MASK indicating
   iff `x` is greater than `y`. Returns `kd.present` when `x` is greater and
   `kd.missing` when `x` is less than or equal to `y`.
 
   Args:
-    x: DataSlice | DataItem.
-    y: DataSlice | DataItem.
+    x: DataSlice.
+    y: DataSlice.
   """
   raise NotImplementedError('implemented in the backend')
 
@@ -104,15 +130,15 @@ def greater(x, y):  # pylint: disable=unused-argument
     qtype_inference_expr=qtypes.DATA_SLICE,
 )
 def less(x, y):  # pylint: disable=unused-argument
-  """Returns true iff `x` is less than `y`.
+  """Returns present iff `x` is less than `y`.
 
   Pointwise operator which takes a DataSlice and returns a MASK indicating
   iff `x` is less than `y`. Returns `kd.present` when `x` is less and
   `kd.missing` when `x` is greater than or equal to `y`.
 
   Args:
-    x: DataSlice | DataItem.
-    y: DataSlice | DataItem.
+    x: DataSlice.
+    y: DataSlice.
   """
   raise NotImplementedError('implemented in the backend')
 
@@ -129,15 +155,15 @@ def less(x, y):  # pylint: disable=unused-argument
     qtype_inference_expr=qtypes.DATA_SLICE,
 )
 def greater_equal(x, y):  # pylint: disable=unused-argument
-  """Returns true iff `x` is greater than or equal to `y`.
+  """Returns present iff `x` is greater than or equal to `y`.
 
   Pointwise operator which takes a DataSlice and returns a MASK indicating
   iff `x` is greater than or equal to `y`. Returns `kd.present` when `x` is
   greater than or equal to `y` and `kd.missing` when `x` is less than `y`.
 
   Args:
-    x: DataSlice | DataItem.
-    y: DataSlice | DataItem.
+    x: DataSlice.
+    y: DataSlice.
   """
   raise NotImplementedError('implemented in the backend')
 
@@ -154,14 +180,14 @@ def greater_equal(x, y):  # pylint: disable=unused-argument
     qtype_inference_expr=qtypes.DATA_SLICE,
 )
 def less_equal(x, y):  # pylint: disable=unused-argument
-  """Returns true iff `x` is less than or equal to `y`.
+  """Returns present iff `x` is less than or equal to `y`.
 
   Pointwise operator which takes a DataSlice and returns a MASK indicating
   iff `x` is less than or equal to `y`. Returns `kd.present` when `x` is
   less than or equal to `y` and `kd.missing` when `x` is greater than `y`.
 
   Args:
-    x: DataSlice | DataItem.
-    y: DataSlice | DataItem.
+    x: DataSlice.
+    y: DataSlice.
   """
   raise NotImplementedError('implemented in the backend')
---
+++ py/koladata/operators/tests/comparison_full_equal_test.py
@@ -0,0 +1,157 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from arolla import arolla
+from koladata.exceptions import exceptions
+from koladata.expr import expr_eval
+from koladata.expr import input_container
+from koladata.expr import view
+from koladata.operators import kde_operators
+from koladata.operators import optools
+from koladata.operators.tests.util import qtypes as test_qtypes
+from koladata.testing import testing
+from koladata.types import data_bag
+from koladata.types import data_slice
+from koladata.types import qtypes
+from koladata.types import schema_constants
+
+I = input_container.InputContainer('I')
+kde = kde_operators.kde
+ds = data_slice.DataSlice.from_vals
+bag = data_bag.DataBag.empty
+DATA_SLICE = qtypes.DATA_SLICE
+
+ENTITY_1 = bag().new()
+
+
+QTYPES = frozenset([
+    (DATA_SLICE, DATA_SLICE, DATA_SLICE),
+])
+
+
+class ComparisonFullEqualTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      (ds([1, None, 3, 2]), ds([1, None, 3, 2]), ds(arolla.present())),
+      (ds([1, 1, 3, 2]), ds([1, None, 3, 2]), ds(arolla.missing())),
+      (
+          ds(['a', 'b', 1, None, 1.5]),
+          ds(['a', 'b', 1, None, 1.5]),
+          ds(arolla.present()),
+      ),
+      (
+          ds(['a', 'b', 1, 2, 1.5]),
+          ds(['a', 'b', 1, None, 1.5]),
+          ds(arolla.missing()),
+      ),
+      (ds([ENTITY_1, None]), ds([ENTITY_1, None]), ds(arolla.present())),
+      (ds([ENTITY_1, ENTITY_1]), ds([ENTITY_1, None]), ds(arolla.missing())),
+      # Broadcasting
+      (
+          ds([2, 2, 2]),
+          1,
+          ds(arolla.missing()),
+      ),
+      (
+          ds([1, None, 1]),
+          1,
+          ds(arolla.missing()),
+      ),
+      (
+          ds([1, 1, 1]),
+          1,
+          ds(arolla.present()),
+      ),
+      # Scalar input
+      (ds(1), ds(1), ds(arolla.present())),
+      (ds(1), ds(2), ds(arolla.missing())),
+      (ds(1), ds(1, schema_constants.INT64), ds(arolla.present())),
+      (ds(1), ds(1.0), ds(arolla.present())),
+      (ds('a'), ds('a'), ds(arolla.present())),
+      (ds('a'), ds('b'), ds(arolla.missing())),
+      (ds('a'), ds(b'a'), ds(arolla.missing())),
+      (ds('a'), ds('a', schema_constants.OBJECT), ds(arolla.present())),
+      (ds(1), ds(arolla.missing()), ds(arolla.missing())),
+      (ds(arolla.missing()), ds(arolla.missing()), ds(arolla.present())),
+      (
+          ds(None, schema_constants.INT32),
+          ds(None, schema_constants.INT32),
+          ds(arolla.present()),
+      ),
+      (ENTITY_1, ENTITY_1, ds(arolla.present())),
+  )
+  def test_eval(self, lhs, rhs, expected):
+    result = expr_eval.eval(kde.comparison.full_equal(lhs, rhs))
+    testing.assert_equal(result, expected)
+
+  def test_qtype_signatures(self):
+    self.assertCountEqual(
+        arolla.testing.detect_qtype_signatures(
+            kde.comparison.full_equal,
+            possible_qtypes=test_qtypes.DETECT_SIGNATURES_QTYPES,
+        ),
+        QTYPES,
+    )
+
+  def test_raises_on_incompatible_schemas(self):
+    with self.assertRaisesRegex(
+        exceptions.KodaError,
+        r"""cannot find a common schema for provided schemas
+
+ the common schema\(s\) INT32: INT32
+ the first conflicting schema [0-9a-f]{32}:0: SCHEMA\(\)""",
+    ):
+      expr_eval.eval(kde.comparison.full_equal(ENTITY_1, ds(1)))
+
+    db = data_bag.DataBag.empty()
+    with self.assertRaisesRegex(
+        exceptions.KodaError,
+        r"""cannot find a common schema for provided schemas
+
+ the common schema\(s\) [0-9a-f]{32}:0: SCHEMA\(x=INT32\)
+ the first conflicting schema [0-9a-f]{32}:0: SCHEMA\(\)""",
+    ):
+      expr_eval.eval(kde.comparison.full_equal(db.new(x=1), db.new()))
+
+    with self.assertRaisesRegex(
+        exceptions.KodaError,
+        'cannot find a common schema for provided schemas',
+    ):
+      expr_eval.eval(kde.comparison.full_equal(db.new(x=1), db.obj()))
+
+    with self.assertRaisesRegex(
+        exceptions.KodaError,
+        'cannot find a common schema for provided schemas',
+    ):
+      expr_eval.eval(
+          kde.comparison.full_equal(
+              db.new(x=1), db.obj().with_schema(schema_constants.ITEMID)
+          )
+      )
+
+  def test_view(self):
+    self.assertTrue(
+        view.has_data_slice_view(kde.comparison.full_equal(I.x, I.y))
+    )
+
+  def test_alias(self):
+    self.assertTrue(
+        optools.equiv_to_op(kde.comparison.full_equal, kde.full_equal)
+    )
+
+
+if __name__ == '__main__':
+  absltest.main()
