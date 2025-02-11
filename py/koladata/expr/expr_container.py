d0ca26b540364618d1abbf9c5c18f053d418257a
Mihai Nitu <mikester@google.com>
2025-02-11 06:04:16 -0800

    Move `NamedContainer` to `third_party`. No longer depends on other container objects.
    PiperOrigin-RevId: 725599932
    Change-Id: I7ce61b6a5abf93495aaf421ab7286281cfae2bab

---
+++ py/koladata/expr/expr_container.py
@@ -0,0 +1,79 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Containers for Exprs."""
+
+from typing import Any, Iterator
+from koladata import kd
+from koladata.expr import tracing_mode
+from koladata.operators import kde_operators
+
+kde = kde_operators.kde
+
+
+class NamedContainer:
+  """Container that automatically names Exprs.
+
+  For non-expr inputs, in tracing mode it will be converted to an Expr,
+  while in non-tracing mode it will be stored as is. This allows to use
+  NamedContainer eager code that will later be traced.
+
+  For example:
+    c = kd.ext.expr_container.NamedContainer()
+    c.x_plus_y = I.x + I.y
+    c.x_plus_y  # Returns (I.x + I.y).with_name('x_plus_y')
+    c.foo = 5
+    c.foo  # Returns 5
+  """
+
+  def __init__(self):
+    # Set directly in __dict__ to not call __setattr__
+    self.__dict__['_container'] = {}
+
+  def __iter__(self) -> Iterator[str]:
+    return self._container.__iter__()  # pytype: disable=attribute-error
+
+  def __dir__(self):
+    return [k for k in self]
+
+  def __len__(self) -> int:
+    return len(self._container)  # pytype: disable=attribute-error
+
+  def __contains__(self, key: Any) -> bool:
+    return key in self._container  # pytype: disable=attribute-error
+
+  def __setattr__(self, key: str, value: Any):
+    if key.startswith('_') or key.endswith('_'):
+      raise AttributeError(
+          f'Attempt to set value with reserved key `container.{key}`.\nNames'
+          ' that start or end with an underscore are reserved for class'
+          ' methods.'
+      )
+    if kd.eager.is_expr(value):
+      value = value.with_name(key)
+    elif tracing_mode.is_tracing_enabled():
+      value = kde.with_name(value, key)
+    self._container[key] = value  # pytype: disable=attribute-error
+
+  def __getattr__(self, key: str):
+    try:
+      return self._container[key]
+    except KeyError as e:
+      raise AttributeError(key) from e
+
+  def __delattr__(self, key: str):
+    try:
+      del self._container[key]
+    except KeyError as e:
+      raise AttributeError(key) from e
---
+++ py/koladata/expr/expr_container_test.py
@@ -0,0 +1,136 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from arolla import arolla
+from koladata import kd
+from koladata.expr import expr_container
+from koladata.expr import introspection
+from koladata.expr import tracing_mode
+
+kde = kd.lazy
+I = kd.I
+V = kd.V
+kdf = kd.functor
+
+
+class ExprContainerTest(absltest.TestCase):
+
+  def test_basics(self):
+    c = expr_container.NamedContainer()
+    c.x = I.x
+    c.z = I.y
+    c.sum = I.x + I.y
+
+    self.maxDiff = None
+    self.assertCountEqual(dir(c), ['x', 'z', 'sum'])
+    self.assertEqual(introspection.get_name(c.x), 'x')
+    arolla.testing.assert_expr_equal_by_fingerprint(c.x.node_deps[0], I.x)
+    self.assertEqual(introspection.get_name(c.z), 'z')
+    arolla.testing.assert_expr_equal_by_fingerprint(c.z.node_deps[0], I.y)
+    self.assertEqual(introspection.get_name(c.sum), 'sum')
+    arolla.testing.assert_expr_equal_by_fingerprint(
+        c.sum.node_deps[0], I.x + I.y
+    )
+
+  def test_override_expr_name(self):
+    c = expr_container.NamedContainer()
+    c.y = I.x.with_name('x')
+
+    self.assertCountEqual(dir(c), ['y'])
+    self.assertEqual(introspection.get_name(c.y), 'y')
+    arolla.testing.assert_expr_equal_by_fingerprint(
+        c.y.node_deps[0], I.x.with_name('x')
+    )
+
+  def test_override_node_name(self):
+    c = expr_container.NamedContainer()
+    c.x = I.x
+    c.x = I.y
+
+    self.assertCountEqual(dir(c), ['x'])
+    self.assertEqual(introspection.get_name(c.x), 'x')
+    arolla.testing.assert_expr_equal_by_fingerprint(c.x.node_deps[0], I.y)
+
+  def test_non_expr_value(self):
+    c = expr_container.NamedContainer()
+    c.x = 5
+    self.assertIsInstance(c.x, int)
+    self.assertEqual(c.x, 5)
+
+  def test_non_expr_value_in_tracing_mode(self):
+    c = expr_container.NamedContainer()
+    with tracing_mode.enable_tracing():
+      c.x = 5
+    self.assertIsInstance(c.x, arolla.Expr)
+    self.assertEqual(kd.eval(c.x), 5)
+
+  def test_non_expr_value_tracing(self):
+    def f(x):
+      c = expr_container.NamedContainer()
+      c.original = x
+      c.updated = c.original + 2
+      return c.updated
+
+    fn = kdf.trace_py_fn(f)
+    self.assertEqual(fn(5), 7)
+    arolla.testing.assert_expr_equal_by_fingerprint(
+        introspection.unpack_expr(fn.get_attr('updated')), V.original + 2
+    )
+    arolla.testing.assert_expr_equal_by_fingerprint(
+        introspection.unpack_expr(fn.original), I.x
+    )
+
+  def test_delete_attr(self):
+    c = expr_container.NamedContainer()
+    c.x = 1
+    c.y = 2
+    c.z = 3
+    del c.y
+    self.assertCountEqual(dir(c), ['x', 'z'])
+
+  def test_len(self):
+    c = expr_container.NamedContainer()
+    c.x = 1
+    c.y = 2
+    c.z = 3
+    self.assertLen(c, 3)
+
+  def test_contains(self):
+    c = expr_container.NamedContainer()
+    c.x = 1
+    c.y = 2
+    self.assertIn('x', c)
+    self.assertIn('y', c)
+    self.assertNotIn('z', c)
+
+  def test_attribute_errors(self):
+    c = expr_container.NamedContainer()
+    with self.assertRaisesRegex(AttributeError, 'foo'):
+      _ = c.foo
+    with self.assertRaisesRegex(AttributeError, 'foo'):
+      del c.foo
+
+  def test_reserved_keys(self):
+    c = expr_container.NamedContainer()
+    with self.assertRaisesRegex(AttributeError, '_a'):
+      c._a = 1
+    with self.assertRaisesRegex(AttributeError, 'a_'):
+      c.a_ = 1
+    with self.assertRaisesRegex(AttributeError, '__setattr__'):
+      c.__setattr__ = 1
+
+
+if __name__ == '__main__':
+  absltest.main()
