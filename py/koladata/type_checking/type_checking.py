25ed28f16918996d072cb3e81a297bdfa8bd4273
Mihai Nitu <mikester@google.com>
2025-03-13 05:11:23 -0700

    Add a runtime type checking API for DataSlices
    PiperOrigin-RevId: 736466097
    Change-Id: Ic4e9f5f577de608f522b478d67aad4c2dde0e410

---
+++ py/koladata/type_checking/type_checking.py
@@ -0,0 +1,191 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Runtime type checking for Koda functions."""
+
+import functools
+import inspect
+
+from koladata.expr import tracing_mode
+from koladata.types import data_item
+from koladata.types import data_slice
+from koladata.types import schema_constants
+
+
+def check_inputs(**kw_constraints: data_item.DataItem):
+  """Decorator factory for adding runtime input type checking to Koda functions.
+
+  Resulting decorators will check the schemas of DataSlice inputs of
+  a function at runtime, and raise TypeError in case of mismatch.
+
+  Decorated functions will preserve the original function's signature and
+  docstring.
+
+  Example for primitive schemas:
+
+    @kd.check_inputs(hours=kd.INT32, minutes=kd.INT32)
+    @kd.check_output(kd.STRING)
+    def timestamp(hours, minutes):
+      return kd.to_str(hours) + ':' + kd.to_str(minutes)
+
+    timestamp(ds([10, 10, 10]), kd.ds([15, 30, 45]))  # Does not raise.
+    timestamp(ds([10, 10, 10]), kd.ds([15.35, 30.12, 45.1]))  # raises TypeError
+
+  Example for complex schemas:
+
+    Doc = kd.schema.named_schema('Doc', doc_id=kd.INT64, score=kd.FLOAT32)
+
+    Query = kd.schema.named_schema(
+        'Query',
+        query_text=kd.STRING,
+        query_id=kd.INT32,
+        docs=kd.list_schema(Doc),
+    )
+
+    @kd.check_inputs(query=Query)
+    @kd.check_output(Doc)
+    def get_docs(query):
+      return query.docs[:]
+
+  Args:
+    **kw_constraints: mapping of parameter names to DataItem schemas. Names must
+      match parameter names in the decorated function. Arguments for the given
+      parameters must be DataSlices/DataItems with the corresponding schemas.
+
+  Returns:
+    A decorator that can be used to type annotate a function that accepts
+    DataSlices/DataItem inputs.
+  """
+  for key, value in kw_constraints.items():
+    if value.get_schema() != schema_constants.SCHEMA:
+      raise TypeError(
+          'kd.check_types: invalid constraint: expected constraint for'
+          f' parameter `{key}` to be a schema DataItem, got'
+          f' {value.get_schema()}'
+      )
+
+  def decorate_f(f):
+    signature = inspect.signature(f)
+    # TODO: Support variadic parameters constraints?
+    if any([
+        p.kind is inspect.Parameter.VAR_POSITIONAL
+        or p.kind is inspect.Parameter.VAR_KEYWORD
+        for p in signature.parameters.values()
+    ]):
+      raise TypeError(
+          'kd.check_types does not support variadic parameters in the'
+          ' decorated function'
+      )
+    for key in kw_constraints:
+      if key not in signature.parameters:
+        raise TypeError(
+            f'kd.check_types: parameter name `{key}` does not match any'
+            ' parameter in function signature'
+        )
+
+    def check_inputs_decorator(*args, **kwargs):
+      # TODO: Support traced constraints/assertions.
+      if tracing_mode.is_tracing_enabled():
+        return f(*args, **kwargs)
+      bound_args = signature.bind(*args, **kwargs)
+      bound_args.apply_defaults()
+      for key, type_constraint in kw_constraints.items():
+        # KeyError is not possible as parameter presence was checked during
+        # decoration.
+        arg = bound_args.arguments[key]
+        if not isinstance(arg, (data_item.DataItem, data_slice.DataSlice)):
+          raise TypeError(
+              'kd.check_types: cannot check type on non-DataSlice'
+              f' argument for parameter `{key}`'
+          )
+        if arg.get_schema() != type_constraint:
+          raise TypeError(
+              f'kd.check_types: type mismatch for parameter `{key}`. Expected'
+              f' type {type_constraint}, got {arg.get_schema()}'
+          )
+      return f(*args, **kwargs)
+
+    return functools.wraps(f)(check_inputs_decorator)
+
+  return decorate_f
+
+
+def check_output(output: data_item.DataItem):
+  """Decorator factory for adding runtime output type checking to Koda functions.
+
+  Resulting decorators will check the schema of the DataSlice output of
+  a function at runtime, and raise TypeError in case of mismatch.
+
+  Decorated functions will preserve the original function's signature and
+  docstring.
+
+  Example for primitive schemas:
+
+    @kd.check_inputs(hours=kd.INT32, minutes=kd.INT32)
+    @kd.check_output(kd.STRING)
+    def timestamp(hours, minutes):
+      return kd.to_str(hours) + ':' + kd.to_str(minutes)
+
+    timestamp(ds([10, 10, 10]), kd.ds([15, 30, 45]))  # Does not raise.
+    timestamp(ds([10, 10, 10]), kd.ds([15.35, 30.12, 45.1]))  # raises TypeError
+
+  Example for complex schemas:
+
+    Doc = kd.schema.named_schema('Doc', doc_id=kd.INT64, score=kd.FLOAT32)
+
+    Query = kd.schema.named_schema(
+        'Query',
+        query_text=kd.STRING,
+        query_id=kd.INT32,
+        docs=kd.list_schema(Doc),
+    )
+
+    @kd.check_inputs(query=Query)
+    @kd.check_output(Doc)
+    def get_docs(query):
+      return query.docs[:]
+
+  Args:
+    output: A DataItem schema for the output. Output of the decorated function
+      must be a DataSlice/DataItem with the corresponding schema.
+
+  Returns:
+    A decorator that can be used to annotate a function returning a
+    DataSlice/DataItem.
+  """
+  if output.get_schema() != schema_constants.SCHEMA:
+    raise TypeError(
+        'kd.check_types: invalid constraint: expected constraint for output to'
+        f' be a schema DataItem, got {output.get_schema()}'
+    )
+
+  def decorate_f(f):
+    def check_output_decorator(*args, **kwargs):
+      # TODO: Support traced constraints/assertions.
+      res = f(*args, **kwargs)
+      if not isinstance(res, (data_item.DataItem, data_slice.DataSlice)):
+        raise TypeError(
+            'kd.check_types: expected DataItem/DataSlice output, got'
+            f' {type(res)}'
+        )
+      if res.get_schema() != output:
+        raise TypeError(
+            'kd.check_types: type mismatch for output. Expected'
+            f' type {output}, got {res.get_schema()}'
+        )
+      return res
+
+    return functools.wraps(f)(check_output_decorator)
+
+  return decorate_f
---
+++ py/koladata/type_checking/type_checking_test.py
@@ -0,0 +1,276 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from koladata import kd
+from koladata.testing import testing
+from koladata.type_checking import type_checking
+from koladata.types import data_slice
+
+ds = data_slice.DataSlice.from_vals
+kdf = kd.functor
+
+
+class TypeCheckingTest(absltest.TestCase):
+
+  def test_primitive_input_type(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    def f(x):
+      return x
+
+    testing.assert_equal(f(ds([1, 2, 3])), ds([1, 2, 3]))
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type INT32,'
+        ' got FLOAT32',
+    ):
+      _ = f(ds([1.0, 2, 3]))
+
+  def test_primitive_output_type(self):
+    @type_checking.check_output(kd.INT32)
+    def f(x):
+      return x
+
+    testing.assert_equal(f(ds([1, 2, 3])), ds([1, 2, 3]))
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for output. Expected type INT32, got'
+        ' FLOAT32',
+    ):
+      _ = f(ds([1.0, 2, 3]))
+
+  def test_data_item(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    def f(x):
+      return x
+
+    testing.assert_equal(f(ds(1)), ds(1))
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type INT32,'
+        ' got FLOAT32',
+    ):
+      _ = f(ds(1.0))
+
+  def test_entity_schema(self):
+    person = kd.schema.new_schema(age=kd.INT32, name=kd.STRING)
+
+    @type_checking.check_inputs(x=person)
+    def f(x):
+      return x
+
+    # Assert does not raise.
+    _ = f(person(age=30, name='John'))
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        # TODO: Improve this error message? Although a user may
+        # already improve it by using named schemas.
+        'kd.check_types: type mismatch for parameter `x`. Expected type'
+        ' SCHEMA(age=INT32, name=STRING), got SCHEMA(age=INT32, name=STRING)',
+    ):
+      _ = f(kd.new(age=32, name='Alice'))
+
+  def test_named_entity_schema(self):
+    person = kd.schema.named_schema(
+        'Person', age=kd.INT32, first_name=kd.STRING
+    )
+
+    @type_checking.check_inputs(x=person)
+    def f(x):
+      return x
+
+    # Assert does not raise.
+    _ = f(person(age=30, first_name='John'))
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type'
+        ' Person(age=INT32, first_name=STRING), got SCHEMA(age=INT32,'
+        ' first_name=STRING)',
+    ):
+      _ = f(kd.uu(age=32, first_name='Alice'))
+
+  def test_uu_entity_schema(self):
+    person = kd.schema.uu_schema(age=kd.INT32, name=kd.STRING)
+
+    @type_checking.check_inputs(x=person)
+    def f(x):
+      return x
+
+    # Assert does not raise.
+    _ = f(person(age=30, name='John'))
+    _ = f(kd.uu(age=32, name='Alice'))
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type'
+        ' SCHEMA(age=INT32, name=STRING), got SCHEMA(age=FLOAT32,'
+        ' name=STRING)',
+    ):
+      _ = f(kd.uu(age=32.0, name='Alice'))
+
+  def test_default_argument(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    def f(x=ds([1.0, 2, 3])):
+      return x
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type INT32,'
+        ' got FLOAT32',
+    ):
+      _ = f()
+
+  def test_using_both_shorthands_triggers_inputs_error_first(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    @type_checking.check_output(kd.INT32)
+    def f(x):
+      return x
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type INT32,'
+        ' got FLOAT32',
+    ):
+      _ = f(ds([1.0, 2, 3]))
+
+  def test_list_schemas(self):
+    doc = kd.schema.named_schema('Doc', doc_id=kd.INT64, score=kd.FLOAT32)
+
+    query = kd.schema.named_schema('Query', docs=kd.list_schema(doc))
+
+    @type_checking.check_inputs(query=query)
+    @type_checking.check_output(doc)
+    def get_docs(query):
+      return query.docs[:]
+
+    q = query(
+        docs=[doc(doc_id=1, score=0.5), doc(doc_id=2, score=0.7)],
+    )
+    # Assert does not raise.
+    _ = get_docs(q)
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `query`. Expected type'
+        ' Query(docs=LIST[Doc(doc_id=INT64, score=FLOAT32)]), got INT32',
+    ):
+      _ = get_docs(ds([1, 2, 3]))
+
+  def test_timestamp_example(self):
+
+    @type_checking.check_inputs(hours=kd.INT32, minutes=kd.INT32)
+    @type_checking.check_output(kd.STRING)
+    def timestamp(hours, minutes):
+      return kd.to_str(hours) + ':' + kd.to_str(minutes)
+
+    testing.assert_equal(timestamp(ds([10, 10, 10]), ds([15, 30, 45])),
+                         ds(['10:15', '10:30', '10:45']))
+
+  def test_invalid_input_constraint_error(self):
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: invalid constraint: expected constraint for parameter'
+        ' `x` to be a schema DataItem, got INT32',
+    ):
+
+      @type_checking.check_inputs(x=kd.int32(0))
+      def _(x):
+        return x
+
+  def test_invalid_output_constraint_error(self):
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: invalid constraint: expected constraint for output to'
+        ' be a schema DataItem, got INT32',
+    ):
+
+      @type_checking.check_output(kd.int32(0))
+      def _(x):
+        return x
+
+  def test_variadic_parameter_error(self):
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types does not support variadic parameters in the decorated'
+        ' function',
+    ):
+
+      @type_checking.check_inputs(x=kd.INT32)
+      def _(x, *args):  # pylint: disable=unused-argument
+        return x
+
+  def test_invalid_keyword_constraint_error(self):
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: parameter name `y` does not match any parameter in'
+        ' function signature',
+    ):
+
+      @type_checking.check_inputs(y=kd.INT32)
+      def _(x):
+        return x
+
+  def test_non_data_slice_input_error(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    def f(x):
+      return x
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: cannot check type on non-DataSlice argument for'
+        ' parameter `x`',
+    ):
+      _ = f(1)
+
+  def test_non_data_slice_output_error(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    @type_checking.check_output(kd.INT32)
+    def f(x):  # pylint: disable=unused-argument
+      return 1
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        "kd.check_types: expected DataItem/DataSlice output, got <class 'int'>",
+    ):
+      _ = f(kd.int32(1))
+
+  def test_ignore_in_tracing(self):
+    @type_checking.check_inputs(x=kd.INT32)
+    def f(x):
+      return x
+
+    with self.assertRaisesWithLiteralMatch(
+        TypeError,
+        'kd.check_types: type mismatch for parameter `x`. Expected type INT32,'
+        ' got FLOAT32',
+    ):
+      _ = f(ds([1.0, 2, 3]))
+
+    fn = kdf.fn(f)
+    testing.assert_equal(fn(ds([1.0, 2, 3])), ds([1.0, 2, 3]))
+
+  def test_docstring_preservation(self):
+    def f(x):
+      """Some docstring."""
+      return x
+    decorated_f = type_checking.check_inputs(x=kd.INT32)(f)
+
+    self.assertEqual(f.__doc__, decorated_f.__doc__)
+
+
+if __name__ == '__main__':
+  absltest.main()
