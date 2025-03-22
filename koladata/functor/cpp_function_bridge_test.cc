838adc07bc7900fb5878e8cc0418a4e696186544
Koladata Team <noreply@google.com>
2025-03-22 13:13:46 -0700

    No public description
    PiperOrigin-RevId: 739524653
    Change-Id: Ic96f32611459a0b3f6d8cf792a5d0605f9dd7dca

--- koladata/functor/cpp_function_bridge_test.cc
+++ koladata/functor/cpp_function_bridge_test.cc
@@ -16,7 +16,6 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
 #include "absl/status/status.h"
 #include "absl/status/status_matchers.h"
 #include "absl/status/statusor.h"
@@ -31,6 +30,7 @@
 #include "arolla/qtype/typed_ref.h"
 #include "arolla/qtype/typed_value.h"
 #include "arolla/serialization/encode.h"
+#include "arolla/util/status_macros_backport.h"
 
 namespace koladata::functor {
 namespace {
