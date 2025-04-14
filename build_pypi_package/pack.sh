c090e9f761daaced490643202893b07369c6e88a
Alexander Pronchenkov <axv@google.com>
2025-04-14 09:55:09 -0700

    A build script for PyPI packages for Arolla and Koladata
    The packages are built together, so we have confidence in version compatibility.
    
    We perform the build inside Docker using the ubuntu:22.04 image because it is relatively compatible with Google Colab.
    
    The script is not intended as an ideal permanent solution, as minor changes are needed for virtually every release. For example, Google Colab recently migrated to Python 3.11 and upgraded to protobuf==5.29.
    
    PiperOrigin-RevId: 747460624
    Change-Id: I4eada39437c49b28babc42901e0721991097a92b

---
+++ build_pypi_package/pack.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is designed to be run within a Docker container. It creates
+# a PyPI package from the files prepared by the build.sh script.
+
+set -euxo pipefail
+
+# Generate setup_*.py files
+cat <<EOF | sed "s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g" > /build/pkg/setup_arolla.py
+from setuptools import setup, find_packages
+
+setup(
+    name="arolla",
+    version="{PACKAGE_VERSION}",
+    package_data={"arolla": ["*.so", "*/*.so", "*/*/*.so", "*/*/*/*.so"]},
+    packages=find_packages(include=["arolla", "arolla.*"]),
+    install_requires=[
+        'protobuf>=5.26.1,<6.0.0dev',
+    ],
+    zip_safe=False,
+)
+EOF
+
+cat <<EOF | sed "s/{PACKAGE_VERSION}/$PACKAGE_VERSION/g" > /build/pkg/setup_koladata.py
+from setuptools import setup, find_packages
+
+setup(
+    name="koladata",
+    version="{PACKAGE_VERSION}",
+    package_data={"koladata": [
+        "*.so", "*/*.so", "*/*/*.so", "*/*/*/*.so",
+        "*.js", "*/*.js", "*/*/*.js", "*/*/*/*.js",
+        "*.js.map", "*/*.js.map", "*/*/*.js.map", "*/*/*/*.js.map",
+    ]},
+    packages=find_packages(include=["koladata", "koladata.*"]),
+    install_requires=[
+        'arolla=={PACKAGE_VERSION}',
+        'cloudpickle',
+        'numpy',
+        'pandas',
+        'IPython',
+    ],
+    zip_safe=False,
+)
+EOF
+
+# Build PyPI packages
+cd /build/pkg
+python3 setup_arolla.py clean --all
+python3 setup_arolla.py bdist_wheel --plat-name=manylinux_2_35_x86_64 --python-tag=cp311
+
+python3 setup_koladata.py clean --all
+python3 setup_koladata.py bdist_wheel --plat-name=manylinux_2_35_x86_64 --python-tag=cp311
