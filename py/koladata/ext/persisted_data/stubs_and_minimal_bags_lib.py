9e725a948a0558ad685924dacfbcf3fbfea9538f
Stephan van Staden <svanstaden@google.com>
2025-08-05 06:59:33 -0700

    Incremental data: Add a module with helper functions to create minimal slices and bags.
    The main user will be PersistedIncrementalDataSliceManager.
    
    PiperOrigin-RevId: 791196172
    Change-Id: Id8354b1ad86640d716f1ec20559d195e7d4faeb9

---
+++ py/koladata/ext/persisted_data/stubs_and_minimal_bags_lib.py
@@ -0,0 +1,222 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library for creating stubs and minimal bags.
+
+A stub is essentially a minimal DataSlice. So this module provides functions
+to create minimal slices and bags that are targeted for use by persisted
+incremental dataslices. The PersistedIncrementalDataSliceManager keeps a schema
+separately from the collection of data bags for the managed slice. So the bags
+used in the collection do not have to carry much information about the schema -
+only the bare minimum is kept.
+"""
+
+from koladata import kd
+from koladata.ext.persisted_data import value_presence_util
+
+
+def _check_no_non_primitive_metadata_attributes(
+    schema: kd.types.DataItem,
+) -> None:
+  """Raises a ValueError if the schema has non-primitive metadata attributes."""
+  try:
+    metadata = schema.get_attr('__schema_metadata__')
+  except ValueError:
+    pass
+  else:
+    for attr_name in kd.dir(metadata):
+      attr_value = metadata.get_attr(attr_name)
+      if not attr_value.is_primitive():
+        # We could in principle also support attr_values that are itemids, but
+        # there is no use case for this now, and it complicates the code because
+        # there is no kd.is_itemid() function.
+        raise ValueError(
+            f'schema {schema} has metadata attributes that are not primitives'
+        )
+
+
+def schema_stub(schema: kd.types.DataItem) -> kd.types.DataItem:
+  """Returns a stub for the given schema item.
+
+  The result is not simply schema.stub():
+  * For kd.named_schema instances, the returned stub will keep the name.
+  * It will keep any metadata associated with the schema.
+
+  For the time being, the metadata's attribute values can only be primitives.
+
+  Args:
+    schema: the schema to stub.
+
+  Raises:
+    ValueError: if the schema has metadata with at least one attribute value
+      that is not a primitive.
+
+  Returns:
+    A stub for the given schema with the additional info (if any).
+  """
+  _check_no_non_primitive_metadata_attributes(schema)
+  schema_stub_bag = kd.bag()
+  for attr_name in ['__schema_name__', '__schema_metadata__']:
+    try:
+      schema_stub_bag = kd.bags.updated(
+          schema_stub_bag,
+          kd.attrs(schema, **{attr_name: schema.get_attr(attr_name)}),
+      )
+    except ValueError:
+      pass
+  return schema.stub().with_bag(schema_stub_bag.merge_fallbacks())
+
+
+def _check_is_acceptable_schema_slice(schema_ds: kd.types.DataSlice) -> None:
+  """Raises a ValueError if the schema_ds has non-primitive metadata attributes.
+
+  The check is performed recursively: non-primitive metadata attributes on
+  sub-schemas are also not allowed.
+
+  Args:
+    schema_ds: a DataSlice with a kd.SCHEMA schema.
+
+  Raises:
+    ValueError: if the schema_ds has non-primitive metadata attributes.
+  """
+  seen_items = kd.bag().dict(key_schema=kd.ITEMID, value_schema=kd.MASK)
+
+  def process(schema_item: kd.types.DataItem) -> None:
+    try:
+      itemid = schema_item.get_itemid()
+    except ValueError:
+      return
+    if not kd.has(itemid):
+      return
+    if seen_items[itemid] == kd.present:
+      return
+    seen_items[itemid] = kd.present
+    _check_no_non_primitive_metadata_attributes(schema_item)
+    try:
+      attr_names = kd.dir(schema_item)
+    except ValueError:
+      return
+    else:
+      for attr_name in attr_names:
+        process(schema_item.get_attr(attr_name))
+
+  for schema_item in schema_ds.flatten().L:
+    process(schema_item)
+
+
+def _stubby(
+    ds: kd.types.DataSlice,
+) -> kd.types.DataSlice:
+  """Helper function to create a stub that is useful for minimal bags.
+
+  The minimal bags are created by PersistedIncrementalDataSliceManager while it
+  traverses an update DataSlice according to its schema. For the most part, the
+  stubs that are used to create these minimal bags can be really minimal, in the
+  sense that they are primitives or itemids without any additional information,
+  not even about the schema (the schema is handled separately by using function
+  `schema_stub()` above). So for the most part, this function simply returns
+  ds.stub().no_bag(). However, there are two Koda schemas that can hide a world
+  of complexity behind them and that are opaque in the sense that traversal
+  based on the schema alone cannot capture the complexity in a step-by-step way.
+  These schemas are kd.OBJECT and kd.SCHEMA. The use of kd.OBJECT is forbidden
+  in persisted incremental dataslices. The use of kd.SCHEMA is allowed; it is
+  handled by keeping the full complexity in a "super stub" that is simply
+  ds.extract().
+
+  Args:
+    ds: the DataSlice to stub.
+
+  Raises:
+    ValueError: if ds is a kd.SCHEMA slice where some sub-schema has metadata
+      with at least one attribute value that is not a primitive.
+
+  Returns:
+    The stub slice for the given DataSlice.
+  """
+  if ds.get_schema() == kd.SCHEMA:
+    _check_is_acceptable_schema_slice(ds)
+    return ds.extract()
+  return ds.stub().no_bag()
+
+
+def minimal_bag_associating_list_with_its_items(
+    list_ds: kd.types.DataSlice,
+) -> kd.types.DataBag | None:
+  """Returns a minimal bag associating the given lists with their items.
+
+  Args:
+    list_ds: a DataSlice with a list schema.
+
+  Returns:
+    A minimal bag associating the given lists with their items, or None if
+    list_ds has no list with information about its items.
+  """
+  # Dedup list itemids here with kd.unique() so that the call to
+  # items_stub.implode(itemid=...) below won't complain about duplicate itemids.
+  list_ds = kd.unique(list_ds.no_bag()).with_bag(list_ds.get_bag())
+  mask = value_presence_util.has_info_about_list_items(list_ds)
+  if mask.is_empty():
+    return None
+  items_stub = _stubby(list_ds[:])
+  itemids = list_ds.get_itemid()
+  return (items_stub.implode(itemid=itemids) & mask).extract_bag()
+
+
+def minimal_bag_associating_dict_with_its_keys_and_values(
+    dict_ds: kd.types.DataSlice,
+) -> kd.types.DataBag | None:
+  """Returns a minimal bag associating the given dicts with their keys and values.
+
+  Args:
+    dict_ds: a DataSlice with a dict schema.
+
+  Returns:
+    A minimal bag associating the given dicts with their keys and values, or
+    None if dict_ds has no key-value data.
+  """
+  # Dedup dict itemids here with kd.unique() so that the call to
+  # kd.dict(..., itemid=...) below won't complain about duplicate itemids.
+  dict_ds = kd.unique(dict_ds.no_bag()).with_bag(dict_ds.get_bag())
+  keys = dict_ds.get_keys()
+  if keys.is_empty():
+    return None  # No keys means there is no key-value data.
+  keys_stub = _stubby(keys)
+  values_stub = _stubby(dict_ds.get_values())
+  itemids = dict_ds.get_itemid()
+  return kd.dict(keys_stub, values_stub, itemid=itemids).extract_bag()
+
+
+def minimal_bag_associating_entity_with_its_attr_value(
+    entity_ds: kd.types.DataSlice,
+    attr_name: str,
+) -> kd.types.DataBag | None:
+  """Returns a minimal bag associating the given entities with their attr values.
+
+  Args:
+    entity_ds: a DataSlice with an entity schema.
+    attr_name: the name of an attribute of the entity schema.
+
+  Returns:
+    A minimal bag associating the given entities with their values for `attr`,
+    or None if entity_ds has no entity with a present or removed value for the
+    given attribute.
+  """
+  non_missing_values_mask = kd.has(
+      entity_ds
+  ) & ~value_presence_util.get_missing_mask(entity_ds, attr_name)
+  entity_ds_to_update = entity_ds & non_missing_values_mask
+  if entity_ds_to_update.is_empty():
+    return None
+  values_stub = _stubby(entity_ds_to_update.get_attr(attr_name))
+  return kd.attrs(entity_ds_to_update, **{attr_name: values_stub})
---
+++ py/koladata/ext/persisted_data/stubs_and_minimal_bags_lib_test.py
@@ -0,0 +1,447 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from koladata import kd
+from koladata.ext.persisted_data import stubs_and_minimal_bags_lib
+
+
+class StubsAndMinimalBagsLibTest(absltest.TestCase):
+
+  def test_function_schema_stub(self):
+    for non_struct_schema in [
+        kd.INT32,
+        kd.INT64,
+        kd.FLOAT32,
+        kd.FLOAT64,
+        kd.STRING,
+        kd.BYTES,
+        kd.BOOLEAN,
+        kd.MASK,
+        kd.EXPR,
+        kd.ITEMID,
+        kd.SCHEMA,
+    ]:
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.schema_stub(non_struct_schema),
+          non_struct_schema.stub(),
+      )
+
+    # Struct schemas without names or metadata behave exactly like their .stub()
+    # counterparts:
+    for struct_schema in [
+        kd.schema.new_schema(x=kd.INT32, y=kd.INT64),
+        kd.schema.new_schema(x=kd.INT32, y=kd.INT64, z=kd.named_schema('foo')),
+        kd.dict_schema(kd.INT32, kd.INT64),
+        kd.dict_schema(kd.INT32, kd.named_schema('foo')),
+        kd.list_schema(kd.INT32),
+        kd.list_schema(kd.named_schema('foo')),
+    ]:
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.schema_stub(struct_schema),
+          struct_schema.stub(),
+      )
+
+      # If we the struct schema carries metadata, then schema_stub() will retain
+      # it:
+      struct_schema_with_metadata = struct_schema.enriched(
+          kd.metadata(struct_schema, proto='my.proto.Message', version=123)
+      )
+      with self.assertRaises(AssertionError):
+        kd.testing.assert_equivalent(
+            stubs_and_minimal_bags_lib.schema_stub(struct_schema_with_metadata),
+            struct_schema_with_metadata.stub(),
+        )
+      expected_schema_stub = (
+          struct_schema_with_metadata.stub()
+          .enriched(
+              kd.attrs(
+                  struct_schema_with_metadata,
+                  **{
+                      '__schema_metadata__': kd.core.get_metadata(
+                          struct_schema_with_metadata
+                      ),
+                  }
+              )
+          )
+          .extract()
+      )
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.schema_stub(struct_schema_with_metadata),
+          expected_schema_stub,
+      )
+      kd.testing.assert_equivalent(
+          kd.core.get_metadata(expected_schema_stub).extract(),
+          kd.core.get_metadata(struct_schema_with_metadata).extract(),
+      )
+
+    # Named schemas retain their names in schema_stub():
+    struct_schema_with_name = kd.named_schema('foo', x=kd.INT32, y=kd.INT64)
+    with self.assertRaises(AssertionError):
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.schema_stub(struct_schema_with_name),
+          struct_schema_with_name.stub(),
+      )
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.schema_stub(struct_schema_with_name),
+        # Notice that it retains only the name and not the attributes x, y or
+        # their schemas:
+        kd.named_schema('foo'),
+    )
+    # They also retain any associated metadata:
+    struct_schema_with_name_and_metadata = struct_schema_with_name.enriched(
+        kd.metadata(
+            struct_schema_with_name, proto='my.proto.Message', version=123
+        )
+    )
+    with self.assertRaises(AssertionError):
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.schema_stub(
+              struct_schema_with_name_and_metadata
+          ),
+          struct_schema_with_name_and_metadata.stub(),
+      )
+    expected_schema_stub = (
+        kd.named_schema('foo')
+        .enriched(
+            kd.attrs(
+                kd.named_schema('foo'),
+                **{
+                    '__schema_metadata__': kd.core.get_metadata(
+                        struct_schema_with_name_and_metadata
+                    ),
+                }
+            )
+        )
+        .extract()
+    )
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.schema_stub(
+            struct_schema_with_name_and_metadata
+        ),
+        expected_schema_stub,
+    )
+    kd.testing.assert_equivalent(
+        kd.core.get_metadata(expected_schema_stub).extract(),
+        kd.core.get_metadata(struct_schema_with_name_and_metadata).extract(),
+    )
+
+    with self.subTest('raises_for_non_primitive_metadata_attribute_value'):
+      with self.assertRaisesRegex(
+          ValueError,
+          'schema .* has metadata attributes that are not primitives',
+      ):
+        stubs_and_minimal_bags_lib.schema_stub(
+            struct_schema_with_name.enriched(
+                kd.metadata(
+                    struct_schema_with_name,
+                    proto=kd.named_schema('proto', name=kd.STRING),
+                )
+            )
+        )
+
+  def test_minimal_bag_associating_list_with_its_items(self):
+    item_schema = kd.named_schema('foo', bar=kd.INT32)
+
+    empty_list = kd.list([], item_schema=item_schema)
+    standard_list = kd.list([item_schema.new(bar=i) for i in range(3)])
+    list_with_no_info_about_items = kd.list(
+        [item_schema.new(bar=i + 5) for i in range(3)]
+    ).no_bag()
+    missing_list = None
+
+    list_ds = kd.slice([
+        empty_list,
+        standard_list,
+        list_with_no_info_about_items,
+        missing_list,
+    ])
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.minimal_bag_associating_list_with_its_items(
+            list_ds
+        ),
+        kd.bags.updated(
+            empty_list[:]
+            .no_bag()
+            .implode(itemid=empty_list.get_itemid())
+            .extract_bag(),
+            standard_list[:]
+            .no_bag()
+            .implode(itemid=standard_list.get_itemid())
+            .extract_bag(),
+        ).merge_fallbacks(),
+    )
+    # At the time of writing, list_ds.stub().get_bag() would additionally
+    # associate list_with_no_info_about_items with an empty list of items. That
+    # is arguably a bug that should be fixed. But the next example shows that
+    # even with that fixed, we still don't want to use list.stub() because its
+    # bag can be far too large.
+
+    # A nested list.
+    inner_list = kd.list([1, 2, 3])
+    nested_list = kd.list([inner_list])
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.minimal_bag_associating_list_with_its_items(
+            nested_list
+        ),
+        nested_list[:]
+        .no_bag()
+        .implode(itemid=nested_list.get_itemid())
+        .extract_bag(),
+    )
+    # Notice that nested_list.stub() here retains the full inner list:
+    kd.testing.assert_equivalent(
+        nested_list.stub(),
+        nested_list,
+    )
+    self.assertEqual(nested_list.stub()[:].to_py(), [[1, 2, 3]])
+    # In contrast to that, the minimal bag will not contain the items of the
+    # inner list:
+    self.assertEqual(
+        nested_list.with_bag(
+            stubs_and_minimal_bags_lib.minimal_bag_associating_list_with_its_items(
+                nested_list
+            )
+        )[:].to_py(),
+        [[]],
+    )
+
+    with self.subTest('minimal_bag_is_None'):
+      self.assertIsNone(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_list_with_its_items(
+              kd.slice(
+                  [list_with_no_info_about_items, missing_list],
+                  schema=kd.list_schema(item_schema),
+              )
+          )
+      )
+
+    with self.subTest('with_duplicate_list_itemids'):
+      list_ds = kd.slice([
+          standard_list,
+          standard_list.no_bag(),
+      ])
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_list_with_its_items(
+              list_ds
+          ),
+          standard_list[:]
+          .no_bag()
+          .implode(itemid=standard_list.get_itemid())
+          .extract_bag(),
+      )
+
+    with self.subTest('with_items_that_are_schemas'):
+      list_ds = kd.list([
+          kd.schema.new_schema(x=kd.INT32),
+          kd.INT32,
+          kd.named_schema('foo', bar=kd.INT32),
+      ])
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_list_with_its_items(
+              list_ds
+          ),
+          list_ds.extract_bag(),
+      )
+
+  def test_minimal_bag_associating_dict_with_its_keys_and_values(self):
+    key_schema = kd.STRING
+    value_schema = kd.named_schema('DictValue', bar=kd.INT32)
+    dict_schema = kd.dict_schema(key_schema, value_schema)
+
+    empty_dict = kd.dict({}, schema=dict_schema)
+    value1 = value_schema.new(bar=1)
+    value2 = value_schema.new(bar=2)
+    standard_dict = kd.dict({'key1': value1, 'key2': value2})
+    dict_with_no_info_about_keys_and_values = kd.dict(
+        {'key3': value_schema.new(bar=3)}
+    ).no_bag()
+    missing_dict = None
+
+    dict_ds = kd.slice([
+        empty_dict,
+        standard_dict,
+        dict_with_no_info_about_keys_and_values,
+        missing_dict,
+    ])
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.minimal_bag_associating_dict_with_its_keys_and_values(
+            dict_ds
+        ),
+        kd.dicts.dict_update(
+            standard_dict,
+            kd.slice(['key1', 'key2']),
+            kd.slice([value1.no_bag(), value2.no_bag()]),
+        ),
+    )
+
+    with self.subTest('minimal_bag_is_None'):
+      self.assertIsNone(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_dict_with_its_keys_and_values(
+              kd.slice(
+                  [
+                      empty_dict,
+                      dict_with_no_info_about_keys_and_values,
+                      missing_dict,
+                  ],
+                  schema=dict_schema,
+              )
+          )
+      )
+
+    with self.subTest('with_duplicate_dict_itemids'):
+      dict_ds = kd.slice([
+          standard_dict,
+          standard_dict.no_bag(),
+      ])
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_dict_with_its_keys_and_values(
+              dict_ds
+          ),
+          kd.dicts.dict_update(
+              standard_dict,
+              kd.slice(['key1', 'key2']),
+              kd.slice([value1.no_bag(), value2.no_bag()]),
+          ),
+      )
+
+    with self.subTest('with_values_that_are_schemas'):
+      dict_ds = kd.dict({
+          'key1': kd.schema.new_schema(x=kd.INT32),
+          'key2': kd.INT32,
+          'key3': kd.named_schema('foo', bar=kd.INT32),
+      })
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_dict_with_its_keys_and_values(
+              dict_ds
+          ),
+          dict_ds.extract_bag(),
+      )
+
+  def test_minimal_bag_associating_entity_with_its_attr_value(self):
+    y_schema = kd.named_schema('Y', z=kd.INT32)
+    entity_schema = kd.named_schema('Entity', x=kd.INT32, y=y_schema)
+
+    empty_entity = entity_schema.new()
+    y_value = y_schema.new(z=2)
+    standard_entity = entity_schema.new(x=1, y=y_value)
+    entity_with_no_info_about_attr_values = entity_schema.new(
+        x=3, y=y_schema.new(z=4)
+    ).no_bag()
+    missing_entity = None
+
+    entity_ds = kd.slice([
+        empty_entity,
+        standard_entity,
+        entity_with_no_info_about_attr_values,
+        missing_entity,
+    ])
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+            entity_ds, 'x'
+        ),
+        kd.attrs(standard_entity, x=1),
+    )
+    kd.testing.assert_equivalent(
+        stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+            entity_ds, 'y'
+        ),
+        kd.attrs(standard_entity, y=y_value.no_bag()),
+    )
+
+    with self.subTest('minimal_bag_is_None'):
+      for attr_name in ['x', 'y']:
+        self.assertIsNone(
+            stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+                kd.slice([
+                    empty_entity,
+                    entity_with_no_info_about_attr_values,
+                    missing_entity,
+                ]),
+                attr_name,
+            )
+        )
+
+    with self.subTest('with_duplicate_entity_itemids'):
+      entity_ds = kd.slice([
+          standard_entity,
+          standard_entity.no_bag(),
+      ])
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+              entity_ds, 'x'
+          ),
+          kd.attrs(standard_entity, x=1),
+      )
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+              entity_ds, 'y'
+          ),
+          kd.attrs(standard_entity, y=y_value.no_bag()),
+      )
+
+    with self.subTest('with_attr_values_that_are_schemas'):
+      entity_schema = kd.schema.new_schema(s=kd.SCHEMA)
+
+      recursive_schema = kd.named_schema(
+          'TreeNode',
+          x=kd.INT32,
+      )
+      recursive_schema = recursive_schema.with_attrs(
+          children=kd.list_schema(recursive_schema)
+      )
+      recursive_schema = recursive_schema.enriched(
+          kd.metadata(recursive_schema, proto='my.proto.TreeNode', version=123),
+          kd.metadata(
+              recursive_schema.children,
+              item_proto='my.proto.TreeNodeList',
+              version=456,
+          ),
+      )
+
+      entity_ds = kd.slice([
+          entity_schema.new(),
+          entity_schema.new(s=kd.schema.new_schema(x=kd.INT32)),
+          entity_schema.new(s=kd.INT32),
+          entity_schema.new(s=kd.dict_schema(kd.INT32, kd.INT64)),
+          entity_schema.new(s=kd.named_schema('foo', bar=kd.INT32)),
+          entity_schema.new(s=kd.INT32).no_bag(),
+          entity_schema.new(s=recursive_schema),
+          None,
+      ])
+      kd.testing.assert_equivalent(
+          stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+              entity_ds, 's'
+          ),
+          entity_ds.extract_bag(),
+      )
+
+      recursive_schema = recursive_schema.enriched(
+          kd.metadata(
+              recursive_schema.children,
+              non_primitive_metadata_attribute=kd.new(x=1),
+          ),
+      )
+      entity_ds = kd.slice([entity_schema.new(s=recursive_schema)])
+      with self.assertRaisesRegex(
+          ValueError,
+          'has metadata attributes that are not primitives',
+      ):
+        stubs_and_minimal_bags_lib.minimal_bag_associating_entity_with_its_attr_value(
+            entity_ds, 's'
+        )
+
+
+if __name__ == '__main__':
+  absltest.main()
