a3e9dbfbb9bf31ace84bbc11ca45f0181bf4a657
Koladata Team <noreply@google.com>
2025-10-02 02:13:31 -0700

    No public description
    PiperOrigin-RevId: 814136991
    Change-Id: I36d5631b12e59642bd1a805c06680c4116415a32

--- py/koladata/base/boxing.h
+++ py/koladata/base/boxing.h
@@ -24,6 +24,7 @@
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
 #include "koladata/adoption_utils.h"
 #include "koladata/data_bag.h"
 #include "koladata/data_slice.h"
