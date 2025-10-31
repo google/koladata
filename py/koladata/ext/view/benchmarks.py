21079250b10bbf4995e05222a96235015e235d2c
Petr Mitrichev <petya@google.com>
2025-10-31 08:41:07 -0700

    Make the benchmark more realistic.
    PiperOrigin-RevId: 826502988
    Change-Id: I7aefd1f170c470c2ff33b491ad954fbe12105e96

--- py/koladata/ext/view/benchmarks.py
+++ py/koladata/ext/view/benchmarks.py
@@ -12,174 +12,502 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-r"""Benchmarks for Konstructs.
+r"""Benchmarks for Koda View.
 
 To profile locally:
 
 blaze run --config=benchmark --copt=-DMAX_PROFILE_STACK_DEPTH_SIZE=1024 \
-    //py/koladata/ext/konstructs:benchmarks -- \
-    --benchmark_filter=example_computation_kv/dataset:0 \
+    //py/koladata/ext/view:benchmarks -- \
+    --benchmark_filter=example_computation_kv_vectorized/dataset:2 \
     --benchmark_min_time=3s --cpu_profile=/tmp/prof && pprof --flame /tmp/prof
 
 To compare against checked-in state:
 
-benchy --reference=srcfs //py/koladata/ext/konstructs:benchmarks
+benchy --reference=srcfs //py/koladata/ext/view:benchmarks
 """
 
 import dataclasses
 import google_benchmark
+from koladata import kd
 from koladata import kd_ext
+from koladata.functions import py_conversions
 
 kv = kd_ext.kv
 
 
-def _to_list(x):
-  if isinstance(x, (list, tuple)):
-    return [_to_list(v) for v in x]
-  else:
-    return x
+@dataclasses.dataclass()
+class Details:
+  attr3: str
+  attr4: str = ''
+  attr5: str = ''
+  attr6: str = ''
 
 
-@dataclasses.dataclass
-class Foo:
-  x: int
+@dataclasses.dataclass()
+class Item:
+  attr1: str
+  details: Details
 
 
-@dataclasses.dataclass
-class Bar:
-  z: list[Foo]
+@dataclasses.dataclass()
+class ExtraInfo:
+  attr1: str
+  attr7: int
 
 
-def _example_computation_py_lists(os: list[Bar]) -> list[list[int]]:
-  return [[f.x * f.x for f in o.z] for o in os]
+@dataclasses.dataclass()
+class Group:
+  attr1: str
+  index: int
+  items: list[Item]
 
 
-def _example_computation_py_tuples(
-    os: list[Bar],
-) -> tuple[tuple[int, ...], ...]:
-  # Note: tuple([i for i in j]) is much faster than tuple(i for i in j).
-  return tuple([tuple([f.x * f.x for f in o.z]) for o in os])
+GROUP_SCHEMA = kd.schema_from_py(Group)
 
 
-def _example_computation_py_stepwise_lists(os: list[Bar]) -> list[list[int]]:
-  # This mirrors the operations we'd likely have to do if we had a Python-only
-  # implementation of View.
-  os = list(os)
-  zs = [o.z for o in os]
-  fs = [list(z) for z in zs]
-  xs = [[f.x for f in fl] for fl in fs]
-  res = [[x * x for x in xl] for xl in xs]
-  return res
+@dataclasses.dataclass()
+class Data:
+  items: list[Item]
+  extra_info: list[ExtraInfo]
+  item_groups: list[Group] = dataclasses.field(default_factory=list)
 
 
-def _example_computation_py_stepwise_tuples(
-    os: list[Bar],
-) -> tuple[tuple[int, ...], ...]:
-  # This mirrors the operations we'd likely have to do if we had a Python-only
-  # implementation of View.
-  # Note: tuple([i for i in j]) is much faster than tuple(i for i in j).
-  os = tuple(os)
-  zs = tuple([o.z for o in os])
-  fs = tuple([list(z) for z in zs])
-  xs = tuple([tuple([f.x for f in fl]) for fl in fs])
-  res = tuple([tuple([x * x for x in xl]) for xl in xs])
-  return res
+DATAS_SCHEMA = kd.schema_from_py(list[Data])
 
 
-def _example_computation_kv(os: list[Bar]) -> list[list[int]]:
-  l = kv.view(os)
-  o = l[:].z[:].x
-  return kv.map(lambda x: x * x, o).get()
+def _example_computation_py(datas: list[Data]) -> list[list[Group]]:
+  """An example computation in Python."""
+  for data in datas:
+    items = [x for x in data.items if x.attr1 != 'Skip']
+    grouped = {}
+    for item in items:
+      if item.attr1 in grouped:
+        grouped[item.attr1].append(item)
+      else:
+        grouped[item.attr1] = [item]
+    for attr1, items in grouped.items():
+      fitems = []
+      pcombined = {}
+      for item in items:
+        details = item.details
+        attr3 = details.attr3
+        if attr3 == 'Type4':
+          combined = details.attr4
+        elif attr3 == 'Type5':
+          combined = details.attr5
+        else:
+          combined = details.attr6
+        if combined in pcombined:
+          fitems.append(pcombined[combined])
+        else:
+          fitems.append(item)
+          pcombined[combined] = item
+      grouped[attr1] = fitems
+
+    attr7_dict = {}
+    for info in data.extra_info:
+      attr7_dict[info.attr1] = info.attr7
+
+    index = {}
+    for attr1 in grouped:
+      if attr1 in attr7_dict:
+        index[attr1] = attr7_dict[attr1]
+      elif attr1 == 'Special1':
+        index[attr1] = 101
+      elif attr1 == 'Special2':
+        index[attr1] = 102
+      else:
+        index[attr1] = -1
+
+    item_groups = []
+    for attr1 in grouped:
+      if index[attr1] >= 0:
+        group = Group(
+            attr1=attr1,
+            index=index[attr1],
+            items=grouped[attr1],
+        )
+        item_groups.append(group)
+    item_groups.sort(key=lambda x: x.index)
+    data.item_groups = item_groups
+
+  return [data.item_groups for data in datas]
 
 
-def _very_small_dataset() -> list[Bar]:
-  return [Bar(z=[Foo(x=57)])]
+def _example_computation_kv_vectorized(datas: list[Data]) -> list[list[Group]]:
+  """An example vectorized computation in Konstructs."""
+  data = kv.view(datas)[:]
+  item = data.items[:]
+  item &= item.attr1 != 'Skip'
+  item = item.group_by(item.attr1)
+  details = item.details
+  combined = (
+      (details.attr4 & (details.attr3 == 'Type4'))
+      | (details.attr5 & (details.attr3 == 'Type5'))
+      | details.attr6
+  )
+  item = item.group_by(combined).take(0)
+  attr1 = item.attr1.collapse()
+  grouped = item.implode()
+  info = data.extra_info[:]
+  attr7_dict = data.map(lambda _: {})
+  attr7_dict[info.attr1] = info.attr7
+  index = (
+      attr7_dict[attr1]
+      | (101 & (attr1 == 'Special1'))
+      | (102 & (attr1 == 'Special2'))
+      | -1
+  )
+  item_groups = kv.map(Group, attr1=attr1, index=index, items=grouped)
+  item_groups = item_groups.select(item_groups.index >= 0)
+  item_groups = item_groups.map(
+      lambda g: sorted(g, key=lambda x: x.index), ndim=1
+  )[:]
+  return item_groups.get()
 
 
-def _small_dataset() -> list[Bar]:
-  return [Bar(z=[Foo(x=i + j) for i in range(3)]) for j in range(4)]
+def _example_computation_kd_no_py_vectorized(
+    datas: kd.types.DataSlice,
+) -> kd.types.DataSlice:
+  """An example vectorized computation in Koda."""
+  data = datas[:]
+  item = data.items[:]
+  item &= item.attr1 != 'Skip'
+  item = kd.group_by(item, item.attr1)
+  details = item.details
+  combined = (
+      (details.attr4 & (details.attr3 == 'Type4'))
+      | (details.attr5 & (details.attr3 == 'Type5'))
+      | details.attr6
+  )
+  item = kd.group_by(item, combined).take(0)
+  attr1 = kd.collapse(item.attr1)
+  grouped = item.implode()
+  info = data.extra_info[:]
+  attr7_dict = kd.dict(info.attr1, info.attr7)
+  index = (
+      attr7_dict[attr1]
+      | (101 & (attr1 == 'Special1'))
+      | (102 & (attr1 == 'Special2'))
+      | -1
+  )
+  item_groups = kd.new(
+      schema=GROUP_SCHEMA, attr1=attr1, index=index, items=grouped
+  )
+  item_groups = item_groups.select(item_groups.index >= 0)
+  item_groups = kd.sort(item_groups, item_groups.index)
+  return item_groups
 
 
-def _medium_dataset() -> list[Bar]:
-  return [Bar(z=[Foo(x=i + j) for i in range(100)]) for j in range(100)]
+def _example_computation_kd_vectorized(datas: list[Data]) -> list[list[Group]]:
+  """An example vectorized computation in Koda with to/from py conversions."""
+  datas = py_conversions._from_py_v2(datas, schema=DATAS_SCHEMA)  # pylint: disable=protected-access
+  item_groups = _example_computation_kd_no_py_vectorized(datas)
+  return item_groups.to_py(max_depth=-1)
+
+
+_example_computation_kd_no_py_traced_vectorized = kd.fn(
+    _example_computation_kd_no_py_vectorized
+)
+
+
+def _example_computation_kd_no_py_single_bag_vectorized(
+    datas: kd.types.DataSlice,
+) -> kd.types.DataSlice:
+  """An example vectorized computation in Koda using a single mutable bag."""
+  datas = datas.fork_bag()
+  bag = datas.get_bag()
+  data = datas[:]
+  item = data.items[:]
+  item &= item.attr1 != 'Skip'
+  item = kd.group_by(item, item.attr1)
+  details = item.details
+  combined = (
+      (details.attr4 & (details.attr3 == 'Type4'))
+      | (details.attr5 & (details.attr3 == 'Type5'))
+      | details.attr6
+  )
+  item = kd.group_by(item, combined).take(0)
+  attr1 = kd.collapse(item.attr1)
+  grouped = bag.implode(item)
+  info = data.extra_info[:]
+  attr7_dict = bag.dict(info.attr1, info.attr7)
+  index = (
+      attr7_dict[attr1]
+      | (101 & (attr1 == 'Special1'))
+      | (102 & (attr1 == 'Special2'))
+      | -1
+  )
+  item_groups = bag.new(
+      schema=GROUP_SCHEMA, attr1=attr1, index=index, items=grouped
+  )
+  item_groups = item_groups.select(item_groups.index >= 0)
+  item_groups = kd.sort(item_groups, item_groups.index)
+  return item_groups
+
+
+def _very_small_dataset() -> list[Data]:
+  return [
+      Data(
+          items=[
+              Item(attr1='A', details=Details(attr3='Type6', attr6='Value6')),
+          ],
+          extra_info=[
+              ExtraInfo(attr1='A', attr7=1),
+          ],
+      )
+  ]
+
+
+def _very_small_dataset_expected_output() -> list[list[Group]]:
+  return [[
+      Group(
+          attr1='A',
+          index=1,
+          items=[
+              Item(
+                  attr1='A',
+                  details=Details(
+                      attr3='Type6', attr4='', attr5='', attr6='Value6'
+                  ),
+              )
+          ],
+      )
+  ]]
+
+
+def _small_dataset() -> list[Data]:
+  return [
+      Data(
+          items=[
+              Item(attr1='B', details=Details(attr3='Unknown', attr6='Value6')),
+              Item(attr1='C', details=Details(attr3='Type4', attr4='Value4')),
+              Item(attr1='C', details=Details(attr3='Type5', attr5='Value5')),
+              Item(attr1='A', details=Details(attr3='Type4', attr4='Value4_2')),
+              Item(attr1='D', details=Details(attr3='Type5', attr5='Value5_2')),
+              Item(attr1='B', details=Details(attr3='Type6', attr6='Value6_2')),
+              Item(
+                  attr1='Special1',
+                  details=Details(attr3='Type4', attr4='Value4_3'),
+              ),
+              Item(
+                  attr1='Skip', details=Details(attr3='Type5', attr5='Value5_3')
+              ),
+              Item(
+                  attr1='B', details=Details(attr3='Unknown', attr6='Value6_3')
+              ),
+              Item(
+                  attr1='Special1',
+                  details=Details(attr3='Type6', attr6='Value6_4'),
+              ),
+          ],
+          extra_info=[
+              ExtraInfo(attr1='A', attr7=3),
+              ExtraInfo(attr1='B', attr7=1),
+              ExtraInfo(attr1='C', attr7=2),
+          ],
+      )
+  ]
+
+
+def _small_dataset_expected_output() -> list[list[Group]]:
+  return [[
+      Group(
+          attr1='B',
+          index=1,
+          items=[
+              Item(
+                  attr1='B',
+                  details=Details(
+                      attr3='Unknown', attr4='', attr5='', attr6='Value6'
+                  ),
+              ),
+              Item(
+                  attr1='B',
+                  details=Details(
+                      attr3='Type6', attr4='', attr5='', attr6='Value6_2'
+                  ),
+              ),
+              Item(
+                  attr1='B',
+                  details=Details(
+                      attr3='Unknown', attr4='', attr5='', attr6='Value6_3'
+                  ),
+              ),
+          ],
+      ),
+      Group(
+          attr1='C',
+          index=2,
+          items=[
+              Item(
+                  attr1='C',
+                  details=Details(
+                      attr3='Type4', attr4='Value4', attr5='', attr6=''
+                  ),
+              ),
+              Item(
+                  attr1='C',
+                  details=Details(
+                      attr3='Type5', attr4='', attr5='Value5', attr6=''
+                  ),
+              ),
+          ],
+      ),
+      Group(
+          attr1='A',
+          index=3,
+          items=[
+              Item(
+                  attr1='A',
+                  details=Details(
+                      attr3='Type4', attr4='Value4_2', attr5='', attr6=''
+                  ),
+              )
+          ],
+      ),
+      Group(
+          attr1='Special1',
+          index=101,
+          items=[
+              Item(
+                  attr1='Special1',
+                  details=Details(
+                      attr3='Type4', attr4='Value4_3', attr5='', attr6=''
+                  ),
+              ),
+              Item(
+                  attr1='Special1',
+                  details=Details(
+                      attr3='Type6', attr4='', attr5='', attr6='Value6_4'
+                  ),
+              ),
+          ],
+      ),
+  ]]
+
+
+def _medium_dataset() -> list[Data]:
+  return sum([_small_dataset() for _ in range(1000)], [])
+
+
+def _medium_dataset_expected_output() -> list[list[Group]]:
+  return sum([_small_dataset_expected_output() for _ in range(1000)], [])
 
 
 _DATASET_CHOICES = (_very_small_dataset, _small_dataset, _medium_dataset)
+_DATASET_EXPECTED_OUTPUT_CHOICES = (
+    _very_small_dataset_expected_output,
+    _small_dataset_expected_output,
+    _medium_dataset_expected_output,
+)
 
 
 @google_benchmark.register
 @google_benchmark.option.arg_names(['dataset'])
 @google_benchmark.option.dense_range(0, len(_DATASET_CHOICES) - 1)
-def example_computation_py_lists(state):
+def example_computation_py(state):
   """An example simple computation."""
   dataset_fn = _DATASET_CHOICES[state.range(0)]
   ds = dataset_fn()
+  expected_output_fn = _DATASET_EXPECTED_OUTPUT_CHOICES[state.range(0)]
+  expected_output = expected_output_fn()
   # Sanity check
-  assert _to_list(_example_computation_py_lists(ds)) == _to_list(
-      _example_computation_kv(ds)
-  )
+  output = _example_computation_py(ds)
+  assert output == expected_output, output
   while state:
-    _ = _example_computation_py_lists(ds)
+    _ = _example_computation_py(ds)
 
 
 @google_benchmark.register
 @google_benchmark.option.arg_names(['dataset'])
 @google_benchmark.option.dense_range(0, len(_DATASET_CHOICES) - 1)
-def example_computation_py_tuples(state):
+def example_computation_kv_vectorized(state):
   """An example simple computation."""
   dataset_fn = _DATASET_CHOICES[state.range(0)]
   ds = dataset_fn()
-  # Sanity check
-  assert _to_list(_example_computation_py_tuples(ds)) == _to_list(
-      _example_computation_kv(ds)
-  )
+  expected_output_fn = _DATASET_EXPECTED_OUTPUT_CHOICES[state.range(0)]
+  expected_output = expected_output_fn()
+  # Sanity check. We return tuples instead of lists, so we need to convert
+  # for exact equality comparison.
+  output = [
+      [dataclasses.replace(y, items=list(y.items)) for y in x]
+      for x in _example_computation_kv_vectorized(ds)
+  ]
+  assert output == expected_output, output
   while state:
-    _ = _example_computation_py_tuples(ds)
+    _ = _example_computation_kv_vectorized(ds)
 
 
 @google_benchmark.register
 @google_benchmark.option.arg_names(['dataset'])
 @google_benchmark.option.dense_range(0, len(_DATASET_CHOICES) - 1)
-def example_computation_py_stepwise_lists(state):
+def example_computation_kd_vectorized(state):
   """An example simple computation."""
   dataset_fn = _DATASET_CHOICES[state.range(0)]
   ds = dataset_fn()
+  expected_output_fn = _DATASET_EXPECTED_OUTPUT_CHOICES[state.range(0)]
+  expected_output = expected_output_fn()
   # Sanity check
-  assert _to_list(_example_computation_py_stepwise_lists(ds)) == _to_list(
-      _example_computation_kv(ds)
-  )
+  output = _example_computation_kd_vectorized(ds)
+  assert output == expected_output, output
   while state:
-    _ = _example_computation_py_stepwise_lists(ds)
+    _ = _example_computation_kd_vectorized(ds)
 
 
 @google_benchmark.register
 @google_benchmark.option.arg_names(['dataset'])
 @google_benchmark.option.dense_range(0, len(_DATASET_CHOICES) - 1)
-def example_computation_py_stepwise_tuples(state):
+def example_computation_kd_no_py_vectorized(state):
   """An example simple computation."""
   dataset_fn = _DATASET_CHOICES[state.range(0)]
   ds = dataset_fn()
+  expected_output_fn = _DATASET_EXPECTED_OUTPUT_CHOICES[state.range(0)]
+  expected_output = expected_output_fn()
+  kd_ds = py_conversions._from_py_v2(ds, schema=DATAS_SCHEMA)  # pylint: disable=protected-access
   # Sanity check
-  assert _to_list(_example_computation_py_stepwise_tuples(ds)) == _to_list(
-      _example_computation_kv(ds)
-  )
+  output = _example_computation_kd_no_py_vectorized(kd_ds).to_py(max_depth=-1)
+  assert output == expected_output, output
   while state:
-    _ = _example_computation_py_stepwise_tuples(ds)
+    _ = _example_computation_kd_no_py_vectorized(kd_ds)
 
 
 @google_benchmark.register
 @google_benchmark.option.arg_names(['dataset'])
 @google_benchmark.option.dense_range(0, len(_DATASET_CHOICES) - 1)
-def example_computation_kv(state):
+def example_computation_kd_no_py_traced_vectorized(state):
   """An example simple computation."""
   dataset_fn = _DATASET_CHOICES[state.range(0)]
   ds = dataset_fn()
+  expected_output_fn = _DATASET_EXPECTED_OUTPUT_CHOICES[state.range(0)]
+  expected_output = expected_output_fn()
+  kd_ds = py_conversions._from_py_v2(ds, schema=DATAS_SCHEMA)  # pylint: disable=protected-access
   # Sanity check
-  assert _to_list(_example_computation_py_lists(ds)) == _to_list(
-      _example_computation_kv(ds)
+  output = _example_computation_kd_no_py_traced_vectorized(kd_ds).to_py(
+      max_depth=-1
   )
+  assert output == expected_output, output
   while state:
-    _ = _example_computation_kv(ds)
+    _ = _example_computation_kd_no_py_traced_vectorized(kd_ds)
+
+
+@google_benchmark.register
+@google_benchmark.option.arg_names(['dataset'])
+@google_benchmark.option.dense_range(0, len(_DATASET_CHOICES) - 1)
+def example_computation_kd_no_py_single_bag_vectorized(state):
+  """An example simple computation."""
+  dataset_fn = _DATASET_CHOICES[state.range(0)]
+  ds = dataset_fn()
+  expected_output_fn = _DATASET_EXPECTED_OUTPUT_CHOICES[state.range(0)]
+  expected_output = expected_output_fn()
+  kd_ds = py_conversions._from_py_v2(ds, schema=DATAS_SCHEMA)  # pylint: disable=protected-access
+  # Sanity check
+  output = _example_computation_kd_no_py_single_bag_vectorized(kd_ds).to_py(
+      max_depth=-1
+  )
+  assert output == expected_output, output
+  while state:
+    _ = _example_computation_kd_no_py_single_bag_vectorized(kd_ds)
 
 
 if __name__ == '__main__':
