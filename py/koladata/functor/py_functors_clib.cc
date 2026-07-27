57b5e57594a3e7bac78acbf5095648a40a2bd4f8
Olga Silina <olgasilina@google.com>
2026-07-27 07:41:25 -0700

    Refactor koladata py_functors into pybind11 module
    This commit removes the handwritten Python C API wrapper and replaces it with Pybind11. There is no change in behavior or public API.
    
    PiperOrigin-RevId: 954622988
    Change-Id: Idfb11d6fcca255e341a02c6409ee0e1f06f5f3c0

---
+++ py/koladata/functor/py_functors_clib.cc
@@ -0,0 +1,70 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#include <vector>
+
+#include "absl/container/flat_hash_set.h"
+#include "arolla/util/fingerprint.h"
+#include "koladata/data_slice.h"
+#include "koladata/functor/auto_variables.h"
+#include "py/arolla/abc/pybind11_utils.h"
+#include "py/arolla/py_utils/py_utils.h"
+#include "py/koladata/base/wrap_utils.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
+
+namespace koladata::python {
+namespace {
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(py_functors_py_ext, m) {
+  m.doc() = "The module with Python bindings for Koda functors.";
+
+  m.def(
+      "auto_variables",
+      [](py::object py_fn,
+         const std::vector<arolla::Fingerprint>& extra_nodes_to_extract_py) {
+        const DataSlice* fn = UnwrapDataSlice(py_fn.ptr(), "fn");
+        if (fn == nullptr) {
+          throw py::error_already_set();
+        }
+        absl::flat_hash_set<arolla::Fingerprint> extra_nodes_to_extract(
+            extra_nodes_to_extract_py.begin(), extra_nodes_to_extract_py.end());
+        DataSlice res = arolla::python::pybind11_unstatus_or(
+            functor::AutoVariables(*fn, std::move(extra_nodes_to_extract)));
+        return arolla::python::pybind11_steal_or_throw<py::object>(
+            WrapPyDataSlice(std::move(res)));
+      },
+      py::arg("fn"),
+      py::arg("extra_nodes_to_extract") = std::vector<arolla::Fingerprint>{},
+      "Returns a functor with auto-variables extracted.");
+
+  m.def(
+      "get_variable_evaluation_order",
+      [](py::object py_fn) {
+        const DataSlice* fn = UnwrapDataSlice(py_fn.ptr(), "fn");
+        if (fn == nullptr) {
+          throw py::error_already_set();
+        }
+        return arolla::python::pybind11_unstatus_or(
+            functor::GetVariableEvaluationOrder(*fn));
+      },
+      py::arg("fn"),
+      "Returns the topologically-sorted variable evaluation order of a "
+      "functor.");
+}
+
+}  // namespace
+}  // namespace koladata::python
