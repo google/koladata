9dff883e691f1ee4d38b9ce971e29aa0a45c0c56
Mihai Nitu <mikester@google.com>
2026-09-25 08:03:51 -0700

    Define DataSlice as `_data_slice.DataSlice` in `kd_types.pyi`
    PiperOrigin-RevId: 988283487
    Change-Id: Idc22d8da32a0886380814496959f387d9b9eef26

--- py/koladata/kd_types.pyi
+++ py/koladata/kd_types.pyi
@@ -15,6 +15,7 @@
 from typing import Any
 
 from koladata.functor.parallel import clib as _functor_parallel_clib
+from koladata.types import data_slice as _data_slice
 from koladata.types import iterable_qvalue as _iterable_qvalue
 from koladata.types import jagged_shape as _jagged_shape
 
@@ -22,7 +23,7 @@ from koladata.types import jagged_shape as _jagged_shape
 # into proper types.
 
 DataBag: Any
-DataSlice: Any
+DataSlice = _data_slice.DataSlice
 DataItem = DataSlice
 DictItem = DataSlice
 Executor = _functor_parallel_clib.Executor
